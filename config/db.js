ed173c2f4dc8a5fcc53e7fdce12f4055f69ab459
HimanshuSwami-Developer <himanshuswami2810@gmail.com>
2023-08-27 20:23:46 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect({
-      // useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // useCreateIndex: true,
-    },process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
       // await parseAndLoadPlanetsData();
     console.log(
       `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
